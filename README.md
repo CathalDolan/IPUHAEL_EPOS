b72dc9af9801953d423ec724fb37050c26763203
CathalDolan <cathal@gizagig.com>
2023-11-10 20:19:59 +0000

    Installed Postgres

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-Shopping Basket
-- Video 1: https://www.youtube.com/watch?v=2G9j34jz42Q
-- Video 2: https://www.youtube.com/watch?v=IH0d1rLVW3s
-- Video 3: https://www.youtube.com/watch?v=E7dDIN-tElQ
-- Video 4: https://www.youtube.com/watch?v=BaUWYZm9T0c
-- Video 5: https://www.youtube.com/watch?v=bQuggmgIEEs
+backports.zoneinfo==0.2.1;python_version<"3.9"
--- ipuhael_epos/settings.py
+++ ipuhael_epos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -99,11 +100,15 @@ WSGI_APPLICATION = 'ipuhael_epos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
