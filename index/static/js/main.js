a5eec4d611cb30f92d4737c46cad4dfe822e2f5d
CathalDolan <cathal@gizagig.com>
2023-11-27 22:48:08 +0000

    UPdated gift names to match the database names

--- index/static/js/main.js
+++ index/static/js/main.js
@@ -1,7 +1,7 @@
 console.log("JS is Working")
 
 let url = "https://ipuhael-epos-8b5f0c382be3.herokuapp.com/";
-// let url = "https://8000-cathaldolan-ipuhaelepos-e452p7jqft4.ws-eu106.gitpod.io/";
+// let url = "https://8000-cathaldolan-ipuhaelepos-3mipea1rgm3.ws-eu106.gitpod.io/";
 
 //SET TIME & DATE: Fn to set time and date.
 window.onload = function() {
