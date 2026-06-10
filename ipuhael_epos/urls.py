dba1f7ed03eafe486a7fc9ffb2630a6f298bae32
PeterKellett <52170335+PeterKellett@users.noreply.github.com>
2026-06-10 12:22:05 +0100

    Homepage redirect to version_2 applied

--- ipuhael_epos/urls.py
+++ ipuhael_epos/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView  # 1. Import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('index.urls')),
+    # path('', include('index.urls')),
+    # 2. Replace index.urls with a RedirectView pointing to version_2/
+    path('', RedirectView.as_view(url='version_2/', permanent=False)),
     path('version_2/', include('version_2.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
